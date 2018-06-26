4cb36d7b84209a1f333b70b2061ab9cea2585d9a
lucperkins <lucperkins@gmail.com>
2018-06-25 20:57:11 -0700

    add complete go example

---
+++ go-example/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/unrolled/render"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/go-chi/chi"
+)
+
+type handler struct {
+	r *render.Render
+}
+
+func newHandler() *handler {
+	return &handler{
+		r: render.New(),
+	}
+}
+
+func (h *handler) get(w http.ResponseWriter, r *http.Request) {
+	h.r.Text(w, http.StatusOK, "Hello world")
+}
+
+func main() {
+	r := chi.NewRouter()
+
+	h := newHandler()
+
+	log.Print("Starting up the server on port 2112")
+
+	r.Get("/", h.get)
+	r.Handle("/metrics", promhttp.Handler())
+
+	log.Fatal(http.ListenAndServe(":2112", r))
+}
