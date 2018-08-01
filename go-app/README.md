97d0683ebd4960b4cd3ce1dd060262cd8a586b7b
lucperkins <lucperkins@gmail.com>
2018-08-01 13:57:15 -0700

    More info in Go app README

--- go-app/README.md
+++ go-app/README.md
@@ -1,3 +1,24 @@
 # Instrumented Go application
 
-In this sandbox, a Prometheus intances scrapes metrics from a simple [Go](https://golang.org)
+In this sandbox, a Prometheus intance scrapes metrics from a simple [Go](https://golang.org) web application.
+
+## Usage
+
+## Usage
+
+To start the sandbox:
+
+```bash
+# In the foreground
+make run # docker-compose up --build
+
+# In detached mode
+make run-detached # docker-compose up --build --detach
+```
+
+This will start up two services:
+
+Service | Description
+:-------|:-----------
+`prometheus` | A Prometheus instance that's [configured](./prometheus/prometheus.yml) to scrape metrics from the Go application running on port 2112
+`myapp` | A simple Go web application that pretty much only exports a simple metric (a `myapp_processed_ops_total` counter that is [incremented](./myapp/main.go#L27-L34) every 2 seconds)
--- go-app/myapp/main.go
+++ go-app/myapp/main.go
@@ -6,36 +6,35 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus/promauto"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
-	PORT = 2112
+	port = 2112
 )
 
 var (
-	queuedOps = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "myapp_ops_queued",
-		Help: "Something or other",
+	processedOps = promauto.NewCounter(prometheus.CounterOpts{
+		Name:      "processed_ops_total",
+		Help:      "The total number of processed events",
+		Namespace: "myapp",
 	})
 )
 
 func recordMetrics() {
 	go func() {
 		for {
-			queuedOps.Add(1)
+			processedOps.Inc()
 			time.Sleep(2 * time.Second)
 		}
 	}()
 }
 
-func init() {
-	prometheus.MustRegister(queuedOps)
-}
-
 func main() {
-	address := fmt.Sprintf(":%d", PORT)
+	address := fmt.Sprintf(":%d", port)
 
 	recordMetrics()
 
